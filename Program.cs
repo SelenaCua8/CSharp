d3c107113e1adcb6e1caf9dd4d9a2fcfd43d468d
SelenaCua8 <101345159+SelenaCua8@users.noreply.github.com>
2023-08-07 12:50:04 -0300

    Día 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace desadiodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor ingrese un texto: ");
+            string ingreso = Console.ReadLine();
+            Console.WriteLine("ingrese el caracterque quiere buscar en el texto: ");
+            char ingresoBuscado = Console.ReadLine()[0];
+            int buscarIndice = ingreso.IndexOf(ingresoBuscado);
+            Console.WriteLine("EL indice del caracter {0} en el texto es {1}", ingresoBuscado, buscarIndice);
+            Console.ReadKey();
+        }
+    }
+}
