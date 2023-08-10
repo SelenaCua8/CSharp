3a40daea3ab5da360f8cbf30ea245badd39a479b
SelenaCua8 <101345159+SelenaCua8@users.noreply.github.com>
2023-08-10 14:45:13 -0300

    Ejercicio de clases

---
+++ clases.cs
@@ -0,0 +1,35 @@
+﻿namespace Clases
+{
+    internal class Humano
+    {
+        public string primerNombre;
+        public string apellido;
+
+        public void presentarme()
+        {
+            Console.WriteLine($"Hola, mi nombre es {primerNombre} {apellido}.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Humano laura = new Humano();
+            Humano franco = new Humano();
+            laura.primerNombre = "Laura";
+            franco.primerNombre = "Franco";
+            laura.apellido = "Lopez";
+            franco.apellido= "Milano";
+
+            laura.presentarme();
+            franco.presentarme();
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
