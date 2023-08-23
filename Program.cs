2e2375b841b7758bbccf8a3733ae1ee99e6b317f
SelenaCua8 <101345159+SelenaCua8@users.noreply.github.com>
2023-08-23 10:17:19 -0300

    Add files via upload
    Como leer y agregar textos

--- Program.cs
+++ Program.cs
@@ -1,16 +1,62 @@
-﻿namespace desadiodos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LeerTexto
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Por favor ingrese un texto: ");
-            string ingreso = Console.ReadLine();
-            Console.WriteLine("ingrese el caracterque quiere buscar en el texto: ");
-            char ingresoBuscado = Console.ReadLine()[0];
-            int buscarIndice = ingreso.IndexOf(ingresoBuscado);
-            Console.WriteLine("EL indice del caracter {0} en el texto es {1}", ingresoBuscado, buscarIndice);
-            Console.ReadKey();
+            //Metodo 1
+            string[] lineas = { "Primero 250", "Segundo 242", "Tercero 240" };
+
+            File.WriteAllLines(@"C:\Users\Win10\Desktop\Textos\Puntajes.txt", lineas);
+
+
+            /*
+            //Metodo 2
+            Console.WriteLine("Por favor ingrese el nombre del archivo");
+            string nombreArchivo = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese el contenido del archivo");
+            string texto = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Win10\Desktop\Textos\" + nombreArchivo + ".txt", texto);
+            */
+
+            //Metodo 3
+            using(StreamWriter archivo = new StreamWriter (@"C:\Users\Win10\Desktop\Textos\miTexto2.txt"))
+            {
+                foreach(string linea in lineas)
+                {
+                    if(linea.Contains("4"))
+                    {
+                        archivo.WriteLine(linea);
+                    }
+                }
+            }
+
+            using (StreamWriter archivo = new StreamWriter (@"C:\Users\Win10\Desktop\Textos\miTexto2.txt", true))
+            {
+                archivo.WriteLine("Linea adicional");
+            }
+
+
+            /*//Leer texto completo
+            string texto = System.IO.File.ReadAllText(@"C:\Users\Win10\Desktop\Textos\LeerArchivoTexto.txt");
+            Console.WriteLine("El archivo contiene el siguiente texto: {0}", texto);
+
+            //Leear de a líneas
+            string[] lineas = System.IO.File.ReadAllLines(@"C:\Users\Win10\Desktop\Textos\LeerArchivoTexto.txt");
+            foreach(string linea in lineas)
+            {
+                Console.WriteLine("\t" + linea);
+            }
+            */
+            Console.Read();
         }
     }
 }
